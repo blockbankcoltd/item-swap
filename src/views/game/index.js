d072d6789ef15c720d4c1be3d3328f3a56191801
Rohit Barnwal <barnwalrohit06@gmail.com>
2022-05-16 11:24:02 +0530

    merger problem fixed

--- src/views/game/index.js
+++ src/views/game/index.js
@@ -100,7 +100,6 @@ const Games = () => {
 
   return (
     <Layout>
-<<<<<<< HEAD
       <section className="flat-title-page inner">
         <div className="themesflat-container">
           <div className="row">
@@ -131,101 +130,6 @@ const Games = () => {
         </div>
       </section>
       {games ? <GameItems title="Explore Games" data={games} /> : <Loader />}
-=======
-      <Swiper
-        modules={[Navigation, Scrollbar, A11y]}
-        spaceBetween={0}
-        slidesPerView={1}
-        navigation
-        scrollbar={{ draggable: true }}
-      >
-        <SwiperSlide className="center">
-          <div
-            className="flat-title-page"
-            style={{ paddingBottom: "20px" }}
-          // style={{ backgroundImage: `url(${imgbg})` }}
-          >
-            <img
-              className="bgr-gradient gradient1"
-              src={Dot_left}
-              alt="Axies"
-            />
-            <img
-              className="bgr-gradient gradient2"
-              src={Dot_right}
-              alt="Axies"
-            />
-            <img
-              className="bgr-gradient gradient3"
-              src={Line_Background}
-              alt="Axies"
-            />
-            <div className="shape item-w-16"></div>
-            <div className="shape item-w-22"></div>
-            <div className="shape item-w-32"></div>
-            <div className="shape item-w-48"></div>
-            <div className="shape style2 item-w-51"></div>
-            <div className="shape style2 item-w-51 position2"></div>
-            <div className="shape item-w-68"></div>
-            <div className="overlay1"></div>
-            <div className="swiper-container mainslider home">
-              <div className="swiper-wrapper">
-                <div className="swiper-slide">
-                  <div className="slider-item">
-                    <div className="themesflat-container ">
-                      <div className="wrap-heading flat-slider flex">
-                        <div className="content">
-                          {/* <h2 className="heading">EXPLORE & STUNNING</h2> */}
-                          <h1 className="heading mb-style">
-                            <span className="tf-text s1">GAME MARKETPLACE</span>
-                          </h1>
-                          {/* <h1 className="heading">MARKETPLACE</h1> */}
-                          {/* <p className="sub-heading">
-                            Marketplace for monster character cllections non
-                            fungible token NFTs
-                          </p> */}
-                          <div
-                            className="flex"
-                            style={{ justifyContent: "center" }}
-                          >
-                            <Link
-                              to="/explore-games"
-                              className="header-slider style style-1 fl-button pri-1"
-                            >
-                              <span>Powered by </span>
-                              <img
-                                src={
-                                  "https://seeklogo.com/images/O/opensea-logo-7DE9D85D62-seeklogo.com.png"
-                                }
-                                width={20}
-                              />
-                              <span> OpenSea</span>
-                            </Link>
-                          </div>
-                        </div>
-                        {/* <div className="image">
-                          <img className="img-bg" src={imgbg1} alt="axies" />
-                          <img src={img1} alt="axies" />
-                        </div> */}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-      </Swiper>
-      {games ? (
-        <GameItems
-          setFilter={filterGames}
-          title={filter !== "all" ? `Showing results for ${filter} games` : "Explore Games"}
-          data={games}
-        />
-      ) : (
-        <Loader />
-      )}
->>>>>>> da93b3c71531e56b33365b1c1f633d27e6686f0d
     </Layout>
   );
 };
